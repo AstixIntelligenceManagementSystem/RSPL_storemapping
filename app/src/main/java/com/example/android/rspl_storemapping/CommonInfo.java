fa782e4e8bce2141cad9e4ade334f60e99cd8a4a
Sunil <astixset2@gmail.com>
2018-05-16 16:39:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/rspl_storemapping/CommonInfo.java
@@ -0,0 +1,270 @@
+package com.example.android.rspl_storemapping;
+
+
+import java.io.File;
+import android.net.Uri;
+public class CommonInfo
+{
+
+
+
+
+
+
+
+	// Its for Live Path on 194 Server
+
+
+
+
+/*
+
+
+	public static File imageF_savedInstance=null;
+    public static String imageName_savedInstance=null;
+    public static String clickedTagPhoto_savedInstance=null;
+    public static Uri uriSavedImage_savedInstance=null;
+ 
+	public static String imei="";
+
+	public static String fileContent="";
+	public static final String MejiDairyPreference="MejiDairyPreference";
+	public static String WebServicePath="http://103.20.212.194/WebServiceAndroidRSPLLive/Service.asmx";
+
+
+	public static int DistanceRange=3000;//Defautl Distance Range to show for store list
+
+    public static String VersionDownloadPath="http://103.20.212.194/downloads/";
+	public static String VersionDownloadAPKName="RSPLSTMapping.apk";
+
+	public static String DATABASE_NAME = "DBAdapterLtFoods";
+
+	public static int AnyVisit = 0;
+
+
+	public static int DATABASE_VERSIONID = 37;      // put this field value based on value in table on the server
+	public static String AppVersionID = "1.13";   // put this field value based on value in table on the server
+	public static int Application_TypeID = 1; //1=Parag Store Mapping,2=Parag SFA Indirect,3=Parag SFA Direct
+
+
+	public static String OrderSyncPath="http://103.20.212.194/ReadXML_RSPLLive/Default.aspx";
+	public static String ImageSyncPath="http://103.20.212.194/ReadXML_RSPLImagesLive/Default.aspx";
+
+
+	public static String RSPLOrderXMLFolder="RSPLXml";
+	public static String RSPLImagesnFolder="RSPLImages";
+	public static String FinalLatLngJsonFile="RSPLFinalLatLngJson";
+
+    public static String AppLatLngJsonFile="RSPLLatLngJson";
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// Its for Testing  Path on 194 server for SFA
+
+
+
+/*
+
+
+	public static File imageF_savedInstance=null;
+
+
+    public static String imageName_savedInstance=null;
+    public static String clickedTagPhoto_savedInstance=null;
+    public static Uri uriSavedImage_savedInstance=null;
+
+    public static String imei="";
+
+
+    public static String fileContent="";
+    public static final String MejiDairyPreference="MejiDairyPreference";
+    public static String WebServicePath="http://103.20.212.194/WebServiceAndroidRSPLTest/Service.asmx";
+    public static String VersionDownloadPath="http://103.20.212.194/downloads/";
+	//public static String VersionDownloadAPKName="RSPLSTMappingDemo.apk";  // imei (865574036956934) HardCode given to Ashish Sir(Alok Sir friend) on 13 march 2018 by Sunil Dangi
+	public static String VersionDownloadAPKName="RSPLSTMappingTest.apk";
+
+	public static String DATABASE_NAME = "DbMeijiSFAApp";
+
+
+	public static int DistanceRange=3000;//Defautl Distance Range to show for store list
+
+	public static int AnyVisit = 0;
+
+
+	public static int DATABASE_VERSIONID = 37; // put this field value based on value in table on the server
+	public static String AppVersionID = "1.14";// put this field value based on value in table on the server
+	public static int Application_TypeID = 1; //1=Parag Store Mapping,2=Parag SFA Indirect,3=Parag SFA Direct
+
+
+	public static String OrderSyncPath="http://103.20.212.194/ReadXML_RSPLTest/Default.aspx";
+	public static String ImageSyncPath="http://103.20.212.194/ReadXML_RSPLImagesTest/Default.aspx";
+
+	public static String RSPLOrderXMLFolder="RSPLXml";
+	public static String RSPLImagesnFolder="RSPLImages";
+	public static String FinalLatLngJsonFile="RSPLFinalLatLngJson";
+	public static String AppLatLngJsonFile="RSPLLatLngJson";
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+	// Its for Development  Path on 194 server for SFA
+
+
+
+
+    public static File imageF_savedInstance=null;
+    public static String imageName_savedInstance=null;
+    public static String clickedTagPhoto_savedInstance=null;
+    public static Uri uriSavedImage_savedInstance=null;
+
+    public static String imei="";
+
+    public static String fileContent="";
+    public static final String MejiDairyPreference="MejiDairyPreference";
+	public static String WebServicePath="http://103.20.212.194/WebServiceAndroidRSPLDevelopment/Service.asmx";
+
+
+	public static int DistanceRange=3000;//Defautl Distance Range to show for store list
+
+
+
+
+    public static String VersionDownloadPath="http://103.20.212.194/downloads/";
+	public static String VersionDownloadAPKName="RSPLSTMappingDev.apk";
+
+	public static String DATABASE_NAME = "DBAdapterLtFoods";
+
+	public static int AnyVisit = 0;
+
+
+	public static int DATABASE_VERSIONID = 12;      // put this field value based on value in table on the server
+	public static String AppVersionID = "1.8";   // put this field value based on value in table on the server
+	public static int Application_TypeID = 1; //1=Parag Store Mapping,2=Parag SFA Indirect,3=Parag SFA Direct
+
+
+	public static String OrderSyncPath="http://103.20.212.194/ReadXML_RSPLDevelopment/Default.aspx";
+	public static String ImageSyncPath="http://103.20.212.194/ReadXML_RSPLImagesDevelopment/Default.aspx";
+
+	public static String RSPLOrderXMLFolder="RSPLXml";
+	public static String RSPLImagesnFolder="RSPLImages";
+	public static String FinalLatLngJsonFile="RSPLFinalLatLngJson";
+    public static String AppLatLngJsonFile="RSPLLatLngJson";
+
+
+    // Its for Test Release Path on 194 server for SFA
+
+
+
+
+/*
+
+    public static File imageF_savedInstance=null;
+    public static String imageName_savedInstance=null;
+    public static String clickedTagPhoto_savedInstance=null;
+    public static Uri uriSavedImage_savedInstance=null;
+
+    public static String imei="";
+
+    public static String fileContent="";
+    public static final String MejiDairyPreference="MejiDairyPreference";
+    public static String WebServicePath="http://103.20.212.194/WebServiceAndroidRSPLTestRelease/Service.asmx";
+
+    public static int DistanceRange=3000;//Defautl Distance Range to show for store list
+
+    public static String VersionDownloadPath="http://103.20.212.194/downloads/";
+    public static String VersionDownloadAPKName="RSPLSTMappingTestRelease.apk";
+
+    public static String DATABASE_NAME = "DBAdapterLtFoods";
+
+    public static int AnyVisit = 0;
+
+    public static int DATABASE_VERSIONID = 33;      // put this field value based on value in table on the server
+    public static String AppVersionID = "1.12";   // put this field value based on value in table on the server
+    public static int Application_TypeID = 1; //1=Parag Store Mapping,2=Parag SFA Indirect,3=Parag SFA Direct
+
+    public static String OrderSyncPath="http://103.20.212.194/ReadXML_RSPLTestRelease/Default.aspx";
+    public static String ImageSyncPath="http://103.20.212.194/ReadXML_RSPLImagesTestRelease/Default.aspx";
+
+    public static String RSPLOrderXMLFolder="RSPLXml";
+    public static String RSPLImagesnFolder="RSPLImages";
+    public static String FinalLatLngJsonFile="RSPLFinalLatLngJson";
+    public static String AppLatLngJsonFile="RSPLLatLngJson";
+*/
+
+
+
+// Its for Training Path on 194 Server
+
+
+/*
+
+	public static File imageF_savedInstance=null;
+	public static String imageName_savedInstance=null;
+	public static String clickedTagPhoto_savedInstance=null;
+	public static Uri uriSavedImage_savedInstance=null;
+
+	public static String imei="";
+
+	public static String fileContent="";
+	public static final String MejiDairyPreference="MejiDairyPreference";
+	public static String WebServicePath="http://103.20.212.194/WebServiceAndroidRSPLTraining/Service.asmx";
+
+
+	public static int DistanceRange=3000;//Defautl Distance Range to show for store list
+
+	public static String VersionDownloadPath="http://103.20.212.194/downloads/";
+	public static String VersionDownloadAPKName="RSPLSTMappingTraining.apk";
+
+	public static String DATABASE_NAME = "DBAdapterLtFoods";
+
+	public static int AnyVisit = 0;
+
+
+	public static int DATABASE_VERSIONID = 29;      // put this field value based on value in table on the server
+	public static String AppVersionID = "1.10";   // put this field value based on value in table on the server
+	public static int Application_TypeID = 1; //1=Parag Store Mapping,2=Parag SFA Indirect,3=Parag SFA Direct
+
+
+	public static String OrderSyncPath="http://103.20.212.194/ReadXML_RSPLTraining/Default.aspx";
+	public static String ImageSyncPath="http://103.20.212.194/ReadXML_RSPLImagesTraining/Default.aspx";
+
+
+	public static String RSPLOrderXMLFolder="RSPLXml";
+	public static String RSPLImagesnFolder="RSPLImages";
+	public static String FinalLatLngJsonFile="RSPLFinalLatLngJson";
+
+	public static String AppLatLngJsonFile="RSPLLatLngJson";
+
+*/
+
+
+
+
+}
